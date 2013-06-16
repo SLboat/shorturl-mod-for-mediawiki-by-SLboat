b27b58ae5dee9b21efda9d2e396fabdc543902f0
SLboat <slboat@me.com>
2013-06-16 10:22:59 +0800

    一个默认的跳转加入
    默认跳转

---
+++ ShortUrl.alias.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * Aliases for special pages
+ *
+ */
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases['en'] = array(
+	'ShortUrl' => array( 'ShortUrl' ),
+);
+
+/** Arabic (العربية) */
+$specialPageAliases['ar'] = array(
+	'ShortUrl' => array( 'مسار_قصير' ),
+);
+
+/** Assamese (অসমীয়া) */
+$specialPageAliases['as'] = array(
+	'ShortUrl' => array( 'চমু_ইউ.আৰ.এল.' ),
+);
+
+/** Banjar (Bahasa Banjar) */
+$specialPageAliases['bjn'] = array(
+	'ShortUrl' => array( 'UrlHandap' ),
+);
+
+/** German (Deutsch) */
+$specialPageAliases['de'] = array(
+	'ShortUrl' => array( 'Kurze_URL' ),
+);
+
+/** Zazaki (Zazaki) */
+$specialPageAliases['diq'] = array(
+	'ShortUrl' => array( 'GreyoKılm' ),
+);
+
+/** Persian (فارسی) */
+$specialPageAliases['fa'] = array(
+	'ShortUrl' => array( 'نشانی_کوتاه' ),
+);
+
+/** French (français) */
+$specialPageAliases['fr'] = array(
+	'ShortUrl' => array( 'UrlCourt', 'Url_court' ),
+);
+
+/** Swiss German (Alemannisch) */
+$specialPageAliases['gsw'] = array(
+	'ShortUrl' => array( 'Churzi_URL' ),
+);
+
+/** Interlingua (interlingua) */
+$specialPageAliases['ia'] = array(
+	'ShortUrl' => array( 'URL_curte' ),
+);
+
+/** Indonesian (Bahasa Indonesia) */
+$specialPageAliases['id'] = array(
+	'ShortUrl' => array( 'UrlPendek' ),
+);
+
+/** Italian (italiano) */
+$specialPageAliases['it'] = array(
+	'ShortUrl' => array( 'UrlCorto' ),
+);
+
+/** Japanese (日本語) */
+$specialPageAliases['ja'] = array(
+	'ShortUrl' => array( '短縮URL' ),
+);
+
+/** Korean (한국어) */
+$specialPageAliases['ko'] = array(
+	'ShortUrl' => array( '짧은Url', '짧은url', '짧은URL' ),
+);
+
+/** Cornish (kernowek) */
+$specialPageAliases['kw'] = array(
+	'ShortUrl' => array( 'UrlCot' ),
+);
+
+/** Luxembourgish (Lëtzebuergesch) */
+$specialPageAliases['lb'] = array(
+	'ShortUrl' => array( 'Kuerz_URL' ),
+);
+
+/** Macedonian (македонски) */
+$specialPageAliases['mk'] = array(
+	'ShortUrl' => array( 'КраткаUrl' ),
+);
+
+/** Malayalam (മലയാളം) */
+$specialPageAliases['ml'] = array(
+	'ShortUrl' => array( 'ചെറുയൂആർഎൽ' ),
+);
+
+/** Norwegian Bokmål (norsk bokmål) */
+$specialPageAliases['nb'] = array(
+	'ShortUrl' => array( 'Kurt_URL' ),
+);
+
+/** Low Saxon (Netherlands) (Nedersaksies) */
+$specialPageAliases['nds-nl'] = array(
+	'ShortUrl' => array( 'Kort_webadres' ),
+);
+
+/** Dutch (Nederlands) */
+$specialPageAliases['nl'] = array(
+	'ShortUrl' => array( 'KorteURL' ),
+);
+
+/** Punjabi (ਪੰਜਾਬੀ) */
+$specialPageAliases['pa'] = array(
+	'ShortUrl' => array( 'ਛੋਟਾ_ਯੂ_ਆਰ_ਐੱਲ' ),
+);
+
+/** Sicilian (sicilianu) */
+$specialPageAliases['scn'] = array(
+	'ShortUrl' => array( 'UrlCorto' ),
+);
+
+/** Vietnamese (Tiếng Việt) */
+$specialPageAliases['vi'] = array(
+	'ShortUrl' => array( 'URL_ngắn' ),
+);
+
+/** Simplified Chinese (中文（简体）‎) */
+$specialPageAliases['zh-hans'] = array(
+	'ShortUrl' => array( '短链接' ),
+);
+
+/** Traditional Chinese (中文（繁體）‎) */
+$specialPageAliases['zh-hant'] = array(
+	'ShortUrl' => array( '縮寫的URL' ),
+);
---
+++ ShortUrl.hooks.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Hooks for ShortUrl for adding link to toolbox
+ *
+ * @file
+ * @ingroup Extensions
+ * @author Yuvi Panda, http://yuvi.in
+ * @copyright © 2011 Yuvaraj Pandian (yuvipanda@yuvi.in)
+ * @licence Modified BSD License
+ */
+
+class ShortUrlHooks {
+	/**
+	 * @param $router PathRouter
+	 * @return bool
+	 *
+	 * Adds ShortURL rules to the URL router.
+	 */
+	public static function setupUrlRouting( $router ) {
+		global $wgShortUrlTemplate;
+		if ( $wgShortUrlTemplate ) {
+			$router->add( $wgShortUrlTemplate,
+				array( 'title' => SpecialPage::getTitleFor( 'ShortUrl', '$1' )->getPrefixedText() )
+			);
+		}
+		return true;
+	}
+
+	/**
+	 * @param $tpl SkinTemplate
+	 * @return bool
+	 */
+	public static function addToolboxLink( &$tpl ) {
+		global $wgShortUrlTemplate, $wgServer;
+
+		if ( !is_string( $wgShortUrlTemplate ) ) {
+			$urlTemplate = SpecialPage::getTitleFor( 'ShortUrl', '$1' )->getFullUrl();
+		} else {
+			$urlTemplate = $wgServer . $wgShortUrlTemplate;
+		}
+
+		$title = $tpl->getSkin()->getTitle();
+		if ( ShortUrlUtils::needsShortUrl( $title ) ) {
+			$shortId = ShortUrlUtils::encodeTitle( $title );
+			$shortURL = str_replace( '$1', $shortId, $urlTemplate );
+			$html = Html::rawElement( 'li', array( 'id' => 't-shorturl' ),
+				Html::Element( 'a', array(
+					'href' => $shortURL,
+					'title' => wfMessage( 'shorturl-toolbox-title' )->text()
+				),
+				wfMessage( 'shorturl-toolbox-text' )->text() )
+			);
+
+			echo $html;
+		}
+		return true;
+	}
+
+	/**
+	 * @param $out OutputPage
+	 * @param $text string the HTML text to be added
+	 * @return bool
+	 */
+	public static function onOutputPageBeforeHTML( &$out, &$text ) {
+		$title = $out->getTitle();
+		if ( ShortUrlUtils::needsShortUrl( $title ) ) {
+			$out->addModules( 'ext.shortUrl' );
+		}
+		return true;
+	}
+
+	/**
+	 * @param $du DatabaseUpdater
+	 * @return bool
+	 */
+	public static function setupSchema( DatabaseUpdater $du ) {
+		$base = __DIR__ . '/schemas';
+		$du->addExtensionTable( 'shorturls', "$base/shorturls.sql" );
+		return true;
+	}
+}
